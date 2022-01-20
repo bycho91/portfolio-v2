0e74cc80dfdc024eedff3e44c0ac49796ffbec86
Benjamin Cho <71800530+bycho91@users.noreply.github.com>
2022-01-20 14:04:09 -0600

    updated hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,12 +13,11 @@ const Hero = (props) => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
-        Hi there! <br />
+        Howdy! <br />
         I'm Benjamin.
       </SectionTitle>
       <SectionText>
-        I am a front-end web developer currently on a journey of learning new
-        technologies. Come check out my latest projects!
+        I am a front-end web developer based out of Dallas, Tx. Come see my latest projects to see what I've been working on!
       </SectionText>
       <Link href="#projects">
         <Button>See Projects</Button>
