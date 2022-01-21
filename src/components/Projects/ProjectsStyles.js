4c0681feea49bcd1b5624915272ac6c6d8ac3516
Benjamin Cho <71800530+bycho91@users.noreply.github.com>
2022-01-21 13:43:16 -0600

    Update ProjectsStyles.js

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -9,7 +9,7 @@ export const Img = styled.img`
 
 export const GridContainer = styled.section`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
   padding: 3rem;
   place-items: center;
   column-gap: 2rem;
@@ -25,7 +25,7 @@ export const BlogCard = styled.div`
   border-radius: 10px;
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
-  width: 500px;
+  width: 400px;
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 100%;
   }
